6c99183cae246f3a5e1cce70d53b0ee05296becb
Wajid Daud Tamboli <wajiddaudtamboli@gmail.com>
2026-01-19 14:27:42 +0530

    Remove compiled API files from git to fix Vercel deployment conflicts

--- api/health.d.ts
+++
@@ -1,3 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(req: VercelRequest, res: VercelResponse): Promise<VercelResponse | undefined>;
-//# sourceMappingURL=health.d.ts.map
